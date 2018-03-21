23fb89ada54fd2766ae7ccf74817d89638617f35
Viachaslau Vashchonak <byslavik@gmail.com>
2018-03-20 22:07:53 -0400

    temp

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ router.use(function(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.sendFile('index.html');
 });
 
 
